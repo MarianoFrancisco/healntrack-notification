8f4aef482063b191df4987e6adaed515a26d9646
Mariano Camposeco <mariano1941@outlook.es>
2025-10-29 17:45:04 -0600

    fix: rename service

--- notification-service/src/main/java/com/sa/healntrack/notification_service/notification/application/service/NotificationServiceImpl.java
+++ notification-service/src/main/java/com/sa/healntrack/notification_service/notification/application/service/NotificationServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class NotificationService implements SendNotification {
+public class NotificationServiceImpl implements SendNotification {
 
     private final EmailSender emailSender;
 
